96e2b40526942d48fd070ee6c8b8fbb4c64db50b
peterjahola <jahola.peter@gmail.com>
2022-03-21 22:50:56 +0100

    asd

--- src/hu/tapasztaltak/model/IMaterial.java
+++ src/hu/tapasztaltak/model/IMaterial.java
@@ -1,6 +1,6 @@
 package hu.tapasztaltak.model;
 
-public class IMaterial extends IStealable {
+public interface IMaterial extends IStealable {
 	public abstract Boolean isCompatible(IMaterial m);
 	public void add(Inventory inv) {
 	}
