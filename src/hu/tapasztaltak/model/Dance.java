7f6a73d4c17642996da5f24eefa744b394850594
Burányi Dániel <buranyidani@gmail.com>
2022-03-21 22:33:14 +0100

    Dance javitva

--- src/hu/tapasztaltak/model/Dance.java
+++ src/hu/tapasztaltak/model/Dance.java
@@ -2,6 +2,7 @@ package hu.tapasztaltak.model;
 
 public class Dance extends Agent implements SpecialModifier {
 	public void effect(Virologist v) {
+		//LEGYEN TÁNC
 	}
 	
 	public void spread(Virologist v) {
