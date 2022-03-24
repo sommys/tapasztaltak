5d3d9cf85174eb6d74264b0e1cc1881f0fbd49de
Burányi Dániel <buranyidani@gmail.com>
2022-03-24 22:23:10 +0100

    TestSetup - Virologist moves init megirasa KB

--- src/hu/tapasztaltak/skeleton/TestSetup.java
+++ src/hu/tapasztaltak/skeleton/TestSetup.java
@@ -2,15 +2,21 @@ package hu.tapasztaltak.skeleton;
 
 import hu.tapasztaltak.model.*;
 
-//TODO: DANA
-//ADOMM MEGCSINALOM MAGAM
+import java.util.HashMap;
+
 public class TestSetup {
+    /**
+     * Statikus HashMap az objektumok tárolására
+     */
+    static HashMap<String, Object> storage = new HashMap<String, Object>();
+
     /**
      * Virologist moves init
      * A virológus mozgását bemutatő függvény
      * Létre kell hozni 2 mezőt és egy virológust.
+     * A létrehozott objektumokat beletesszük a HashMapbe.
      * Ezek után az f1 mezőhöz hozzáadjuk a v virolőgust.
-     * A két mezőt beállítjuk egymás szomszédjának.
+     * A virológusnak beállítjuk az f1 mezőt.
      * A virológust az f2 mezőre mozgatjuk.
      */
     public void virologistMoves(){
@@ -18,28 +24,49 @@ public class TestSetup {
         Field f2 = new Field();
         Virologist v = new Virologist();
 
+        storage.put("f1", f1);
+        storage.put("f2", f2);
+        storage.put("v", v);
+
         f1.addVirologists(v);
-        f1.addNeighbours(f2);
 
-        f2.addNeighbours(f1);
+        //ezek nem kellenek akkor
+        // A két mezőt beállítjuk egymás szomszédjának.
+        //f1.addNeighbours(f2);
+        //f2.addNeighbours(f1);
+
+        v.setField(f1);
         v.move(f2);
-    }
 
+        //ezek hová?
+        //storage.remove(v);
+        //storage.remove(f1);
+        //storage.remove(f2);
+    }
 
     /**
      * Virologist uses agent on themself init
      * A virológus használja magán az ágenst
+     * Létre kell hozni 1 inventoryt és egy virológust.
+     * A létrehozott objektumokat beletesszük a HashMapbe.
      */
     public void useAgentOnThemself(){
+        /*
         Gloves g = new Gloves();
         Cape c = new Cape();
         Protect p = new Protect();
         Protect p2 = new Protect();
         Dance d = new Dance();
         Stun s = new Stun();
-        Forget f = new Forget();
+        Forget f = new Forget();*/
 
+        Inventory inv = new Inventory();
+        Virologist v = new Virologist();
 
+        v.setInventory(inv);
+        //inv.setVirologist(v); :(
+
+        storage.put("inv", inv);
+        storage.put("v", v);
     }
-
 }
