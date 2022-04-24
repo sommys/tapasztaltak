df22614dd36b0b0713884b94d730cd6202c78ca2
peterjahola <jahola.peter@gmail.com>
2022-04-24 23:32:14 +0200

    Added ProtoLogger to InfLabor

--- src/hu/tapasztaltak/model/InfLabor.java
+++ src/hu/tapasztaltak/model/InfLabor.java
@@ -1,10 +1,13 @@
 package hu.tapasztaltak.model;
 
-import static hu.tapasztaltak.proto.ProtoMain.getGeneId;
+import hu.tapasztaltak.proto.ProtoLogger;
+
+import static hu.tapasztaltak.proto.ProtoMain.getIdForObject;
 
 public class InfLabor extends Labor{
     @Override
     public void getItem(Virologist v) throws Exception {
+        ProtoLogger.logMessage(String.format("%s tries to infect %s with Bear", getIdForObject(v), getIdForObject(gene)));
         v.learn(gene);
         Bear b = new Bear();
         v.useAgent(b,v);
