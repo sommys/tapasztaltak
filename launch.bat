8d1a567ab0a5b73a081f2ae68da87a480b2cb2a7
sommys <somaseres@gmail.com>
2022-04-25 11:00:58 +0200

    proto launcher

--- launch.bat
+++ launch.bat
@@ -1,6 +1,7 @@
 @echo off
-	javac -encoding utf8 -d classes src/hu/tapasztaltak/model/*.java src/hu/tapasztaltak/skeleton/*.java
+	javac -encoding utf8 -d classes src/hu/tapasztaltak/model/*.java src/hu/tapasztaltak/proto/*.java
+	if not exist classes mkdir classes
 	cd classes
-	jar cfe ../tapasztaltak.jar hu.tapasztaltak.skeleton.SkeletonMenu hu/tapasztaltak
+	jar cfe ../tapasztaltak.jar hu.tapasztaltak.proto.ProtoMain hu/tapasztaltak
 	cd ..
 	java -jar tapasztaltak.jar
